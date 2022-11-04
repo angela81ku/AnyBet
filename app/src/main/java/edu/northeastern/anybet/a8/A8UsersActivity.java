dd455a833223f110ea950f69c6c80a37570ebc43
laimax17 <caicai0660@gmail.com>
2022-11-03 23:46:00 -0400

    create user screen

---
+++ app/src/main/java/edu/northeastern/anybet/a8/A8UsersActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.anybet.a8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.anybet.R;
+
+public class A8UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a8_users);
+    }
+}
