fb68201e39f9178b50c0a91cee5a1576ea550d7d
Jiaxi Wen <taurreswen@gmail.com>
2022-11-04 17:33:22 -0400

    Implement update sticker count after a message is sent

--- app/src/main/java/edu/northeastern/anybet/a8/A8UsersActivity.java
+++ app/src/main/java/edu/northeastern/anybet/a8/A8UsersActivity.java
@@ -1,19 +1,26 @@
 package edu.northeastern.anybet.a8;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.northeastern.anybet.R;
 import edu.northeastern.anybet.a8.realtimeDatabase.DAO.FirebaseDAO;
 import edu.northeastern.anybet.a8.realtimeDatabase.models.Message;
+import edu.northeastern.anybet.a8.realtimeDatabase.models.StickerUser;
 
 public class A8UsersActivity extends AppCompatActivity {
 
@@ -35,6 +42,7 @@ public class A8UsersActivity extends AppCompatActivity {
         a8UserReceiveUsernameInput = findViewById(R.id.a8User_receiveUsername_Input);
 
         dao = new FirebaseDAO();
+        db = FirebaseDatabase.getInstance();
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -46,12 +54,28 @@ public class A8UsersActivity extends AppCompatActivity {
         a8UserSendButton.setOnClickListener(view -> {
             //todo update stickerId
             String recipient = a8UserReceiveUsernameInput.getText().toString();
-            String stickerId = "1";
+            String stickerId = "sticker1";
             Message message = new Message(curUser, recipient, stickerId);
             dao.addMessage(message);
 
-//            // add sticker count
-//            db.getReference("users").equalTo(curUser)
+            // update sticker count
+            db.getReference("users").child(curUser).get()
+                    .addOnCompleteListener(task -> {
+                        StickerUser user = task.getResult().getValue(StickerUser.class);
+                        if (user != null) {
+                            Map<String, Integer> stickerCount = user.getStickerCount();
+                            if (stickerCount != null) {
+                                stickerCount.put(stickerId, stickerCount.getOrDefault(stickerId, 0) + 1);
+                            } else {
+                                stickerCount = new HashMap<>();
+                                stickerCount.put(stickerId, 1);
+                            }
+                            System.out.println(stickerCount);
+                            Map<String, Object> update = new HashMap<>();
+                            update.put("/" + curUser + "/stickerCount", stickerCount);
+                            db.getReference("users").updateChildren(update);
+                        }
+                    });
         });
     }
 }
