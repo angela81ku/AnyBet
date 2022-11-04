88d351a6002bad1a1840f319c5b5d023bdad38de
Jiaxi Wen <taurreswen@gmail.com>
2022-11-03 23:01:31 -0400

    Create DAO

--- app/src/main/java/edu/northeastern/anybet/a8/realtimeDatabase/DAO/FirebaseDAO.java
+++ app/src/main/java/edu/northeastern/anybet/a8/realtimeDatabase/DAO/FirebaseDAO.java
@@ -1,4 +1,27 @@
 package edu.northeastern.anybet.a8.realtimeDatabase.DAO;
 
+import com.google.firebase.database.DatabaseReference;
+
+import edu.northeastern.anybet.a8.realtimeDatabase.models.Message;
+import edu.northeastern.anybet.a8.realtimeDatabase.models.StickerUser;
+
 public class FirebaseDAO {
+
+    private final DatabaseReference databaseReference;
+
+    public FirebaseDAO(DatabaseReference databaseReference) {
+        this.databaseReference = databaseReference;
+    }
+
+    public void addUser(StickerUser user) {
+        if (user != null) {
+            databaseReference.child("users").push().setValue(user);
+        }
+    }
+
+    public void addMessage(Message message) {
+        if (message != null) {
+            databaseReference.child("messages").push().setValue(message);
+        }
+    }
 }
