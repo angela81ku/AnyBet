fdfb2af4d67963f2c0b49995651607803fd38932
calmix <61977333+calmix@users.noreply.github.com>
2022-11-05 21:31:17 -0400

    Revert "Yanxian"

--- app/src/main/java/edu/northeastern/anybet/a8/realtimeDatabase/models/Sticker.java
+++ app/src/main/java/edu/northeastern/anybet/a8/realtimeDatabase/models/Sticker.java
@@ -2,38 +2,24 @@ package edu.northeastern.anybet.a8.realtimeDatabase.models;
 
 public class Sticker {
     private String stickerId;
-    private int count;
-//    private String stickerPath;
+    private String stickerPath;
 
     public Sticker() {
 
     }
 
-    public Sticker(String stickerId, int count) {
+    public Sticker(String stickerId, String stickerPath){
         this.stickerId = stickerId;
-        this.count = count;
+        this.stickerPath = stickerPath;
     }
-
-//    public Sticker(String stickerId, String stickerPath){
-//        this.stickerId = stickerId;
-//        this.stickerPath = stickerPath;
-//    }
 
     public String getStickerId() {
         return stickerId;
     }
 
-    public void setCount(int count) {
-        this.count = count;
+    public String getStickerPath() {
+        return stickerPath;
     }
-
-    public int getCount() {
-        return count;
-    }
-
-//    public String getStickerPath() {
-//        return stickerPath;
-//    }
 
 
 }
