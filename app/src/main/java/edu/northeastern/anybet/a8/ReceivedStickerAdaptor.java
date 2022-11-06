8d94531b70e17cbb4772d07da265783603ebca8c
Yanxian Chen <calvinyx@amazon.com>
2022-11-06 12:47:27 -0500

    handle unknown sticker received

--- app/src/main/java/edu/northeastern/anybet/a8/ReceivedStickerAdaptor.java
+++ app/src/main/java/edu/northeastern/anybet/a8/ReceivedStickerAdaptor.java
@@ -37,8 +37,10 @@ public class ReceivedStickerAdaptor extends RecyclerView.Adapter<ReceivedSticker
             holder.img.setImageResource(R.drawable.sticker1);
         } else if (id.equals("sticker2")) {
             holder.img.setImageResource(R.drawable.sticker2);
-        } else {
+        } else if (id.equals("sticker3")) {
             holder.img.setImageResource(R.drawable.sticker3);
+        } else {
+            holder.img.setImageResource(R.drawable.ic_launcher_background);
         }
     }
 
