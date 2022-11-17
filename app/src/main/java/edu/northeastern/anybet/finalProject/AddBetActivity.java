0412830276de1090b90928b5b90110d5995ecd3a
Yizhen Ku <ku.yi@northeastern.edu>
2022-11-17 01:13:09 -0500

    bet detail page UI structure done

--- app/src/main/java/edu/northeastern/anybet/finalProject/AddBetActivity.java
+++ app/src/main/java/edu/northeastern/anybet/finalProject/AddBetActivity.java
@@ -2,7 +2,10 @@ package edu.northeastern.anybet.finalProject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import edu.northeastern.anybet.R;
 
 public class AddBetActivity extends AppCompatActivity {
@@ -11,5 +14,9 @@ public class AddBetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anybet_add_bet);
+    }
+    public void clickCreateBet(View view){
+        Intent intent = new Intent(this, BetDetailActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/northeastern/anybet/finalProject/BetDetailActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.anybet.finalProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import edu.northeastern.anybet.R;
+public class BetDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anybet_bet_detail);
+    }
+}
